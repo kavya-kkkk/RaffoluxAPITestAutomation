02988b6fc350ca36d89db07b2646585f71666366
kavya-kkkk <126249025+kavya-kkkk@users.noreply.github.com>
2023-06-06 11:15:43 +0530

    Update BaseClass.java

--- src/test/java/com/Utiliies/BaseClass.java
+++ src/test/java/com/Utiliies/BaseClass.java
@@ -16,6 +16,9 @@ public class BaseClass {
 //      public String	REPORTPATH= "C:\\Users\\desktop\\eclipse\\RestAssured\\RafflouxAPI-HTML-Report\\ApiTestPN2.6-Report-Raffloux.html";
 //     public String	REPORTPATHLogin= "C:\\Users\\desktop\\eclipse\\RestAssured\\RafflouxAPI-HTML-Report\\Login2.6ApiTest-Report-Raffloux.html";
  	
- public String	REPORTPATH= "C:\\Users\\desktop\\Desktop\\Jenkins\\ApiTestPN2.6-Report-Raffloux.html";
-    public String	REPORTPATHLogin= "C:\\Users\\desktop\\Desktop\\Jenkins\\Login2.6ApiTest-Report-Raffloux.html";
+//  public String	REPORTPATH= "C:\\Users\\desktop\\Desktop\\Jenkins\\ApiTestPN2.6-Report-Raffloux.html";
+//     public String	REPORTPATHLogin= "C:\\Users\\desktop\\Desktop\\Jenkins\\Login2.6ApiTest-Report-Raffloux.html";
+	
+	public String	REPORTPATH= "C:\Users\desktop\.jenkins\workspace\RafflouxAPITest\RafflouxAPI-HTML-Report\\ApiTestPN2.6-Report-Raffloux.html";
+    public String	REPORTPATHLogin= "C:\Users\desktop\.jenkins\workspace\RafflouxAPITest\RafflouxAPI-HTML-Report\\Login2.6ApiTest-Report-Raffloux.html";
 }
